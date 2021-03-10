df918cd4f94d8a8f085966d0bfaaf1580346d4b2
Bruno Lima <brunopclima81@gmail.com>
2021-03-10 16:06:25 -0300

    removing some projects

--- patrimonio-api/src/main/java/com/algaworks/patrimonio/repository/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.algaworks.patrimonio.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.algaworks.patrimonio.model.Item;
-
-public interface ItemRepository extends JpaRepository<Item, Long> {
-
-}
